776b11572b67fc24e347a4bcdca624060d1e439c
Matias Derlich <mderlich@gmail.com>
2024-03-16 11:29:53 -0300

    subiendo...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { StudentsComponent } from './componentes/students/students.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentsComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
